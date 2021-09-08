ce8325a8d8eab81144b55956db74331c7c3dec06
BhagyaDeepika <bmekala@aadhya-analytics.com>
2021-09-08 17:45:39 +0530

    updated versionsss

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 import codecs
 import os
 
@@ -6,27 +6,29 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Sample testing package'
+LONG_DESCRIPTION = 'A package that allows to build simple applications'
+
+# Setting up
 setup(
-  name = 'seaaddition',         # How you named your package folder (MyLib)
-  packages = ['seaaddition'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'testing data locally',   # Give a short description about your library
-  author = 'Deepika',                   # Type in your name
-  author_email = 'mekalabhagyadeepika@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/BhagyaDeepika/sampleread.git',   # Provide either the link to your github or to your website
-  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          
-          'requests',
-      ],
-  classifiers=[
+    name="seaaddition",
+    version=VERSION,
+    author="Deepika",
+    author_email="<mekalabhagyadeepika@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ],
+    ]
 )
